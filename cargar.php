4e736210a4c97cb1a47cfce8621c551ce38bc38e
Naranjas <duniabolson@gmail.com>
2025-06-10 15:32:40 -0300

    subimos proyecto ejemplo

--- cargar.php
+++ cargar.php
@@ -20,31 +20,25 @@ if (isset($_SESSION['admin'])) {
 ?>
 
   <section class="contenedor_carga">
-    <h3> Cargar personajes</h3>
-    <form action="cargar_personaje.php" method="post" class="formulario" enctype="multipart/form-data">
-      <input type="text" name="nombre" placeholder="Nombre">
-      <input type="text" name="apellido" placeholder="Apellido">
-      <div class="box_select">
-        <input type="file" name="imagen" placeholder="Imagen">
-      </div>
-      <textarea name="descripcion" id="" cols="30" rows="10"></textarea>
-      <div class="box_select">
-        <label for="estado"> Estado: </label>
-        <select name="estado" id="">
-          <option value="proceso"> en proceso</option>
-          <option value="finalizado"> finalizado</option>
-        </select>
-      </div>
-
-      <img src="captcha.php" alt="captcha">
-      <input type="text" name="captcha" placeholder="Ingresa captcha">
-      <input type="submit" value="Cargar Personaje">
+    <h3>Cargar personajes</h3>
+        <form action="cargar_personaje.php" method="post" class="formulario" enctype="multipart/form-data">
+            <input type="text" name="nombre" placeholder="Nombre">
+            <input type="text" name="apellido" placeholder="Apellido">
+            <input type="file" name="imagen" placeholder="Imagen">
+            <textarea name="descripcion" id="" cols="30" rows="10"></textarea>
+            
+            <select name="estado" id="">
+                <option value="proceso"> en proceso</option>
+                <option value="finalizado"> finalizado</option>
+            </select>
+            <img src="captcha.php" alt="captcha">
+            <input type="text" name="captcha" placeholder="Ingresa captcha">
+            <input type="submit" value="Cargar Personaje">
 
 
-    </form>
+        </form>
 
   </section>
-
 <?php
 
   if (isset($_GET['error_codigo'])) {
